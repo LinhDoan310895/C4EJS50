97ad674ad7efff773d4791e96024204d23e9669f
Linh Doan <doanvulinh310895@gmail.com>
2019-09-29 22:59:02 +0700

    hoan thanh btvn 75%

--- Sesssion3_condition/condition_intro.js
+++ Sesssion3_condition/condition_intro.js
@@ -1,27 +1,30 @@
-// let a = parseInt(prompt("Moi nhap vao so a: "));
-// let b = parseInt(prompt("Moi nhap vao so b: "));
-// let c = parseInt(prompt("Moi nhap vao so c: "));
+/*
+let a = parseInt(prompt("Moi nhap vao so a: "));
+let b = parseInt(prompt("Moi nhap vao so b: "));
+let c = parseInt(prompt("Moi nhap vao so c: "));
 
-// let max = a;
+let max = a;
 
-// if (b > max) {
-//     max = b;
-//     if (c > max) {
-//         max = c;
-//         console.log(`So lon nhat trong 3 so la: c, co gia tri la ${max}`);
-//     }
-//     else{
-//         console.log(`So lon nhat trong 3 so la: b, co gia tri la ${max}`);
-//     }
-// }
-// else if (c > max) {
-//     max = c;
-//     console.log(`So lon nhat trong 3 so la: c, co gia tri la ${max}`);
-// }
-// else {
-//     console.log(`So lon nhat trong 3 so la: a, co gia tri la ${max}`);
-// }
+if (b > max) {
+    max = b;
+    if (c > max) {
+        max = c;
+        console.log(`So lon nhat trong 3 so la: c, co gia tri la ${max}`);
+    }
+    else{
+        console.log(`So lon nhat trong 3 so la: b, co gia tri la ${max}`);
+    }
+}
+else if (c > max) {
+    max = c;
+    console.log(`So lon nhat trong 3 so la: c, co gia tri la ${max}`);
+}
+else {
+    console.log(`So lon nhat trong 3 so la: a, co gia tri la ${max}`);
+}
+*/
 
+/*
 let year = parseInt(prompt("Moi nhap vao 1 nam bat ky: "));
 let month = parseInt(prompt(`Moi nhap vao 1 thang trong nam ${year}: `));
 
@@ -47,3 +50,25 @@ else if (month === 4 || month === 6 || month === 9 || month === 11) {
 else {
     console.log(`Gia tri ${month} vua nhap vao khong phai mot thang.`);
 }
+*/
+
+/* 
+const pi = 3.14;
+let banKinh = parseInt(prompt("Moi nhap vao ban kinh hinh tron: "));
+let chuVi = 2 * banKinh * pi;
+let dienTich=banKinh*banKinh*pi;
+console.log(`Hinh tron co ban kinh ${banKinh}`);
+console.log(`Hinh tron co chu vi: ${chuVi}`);
+console.log(`Hinh tron co dien tich: ${dienTich}`);
+
+let chieuDai = parseInt(prompt("Moi nhap vao chieu dai hinh chu nhat: "));
+let chieuRong = parseInt(prompt("Moi nhap vao chieu rong hinh chu nhat: "));
+let chuVi = 2 * (chieuDai + chieuRong);
+let dienTich = chieuDai * chieuRong;
+console.log(`Hinh chu nhat co chu vi: ${chuVi}`);
+console.log(`Hinh chu nhat co dien tich: ${dienTich}`);
+ */
+
+let x = parseFloat(prompt("Moi nhap vao gia tri cua x: "));
+let y1 = 4 * (Math.pow(x, 2) + 10 * x * Math.sqrt(x) + 3 * x + 1);
+let y2=
