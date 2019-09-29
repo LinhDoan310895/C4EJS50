503d476d377c8f4e838042d96a1b1852d6eabf61
Linh Doan <doanvulinh310895@gmail.com>
2019-09-30 00:55:35 +0700

    merged laptop & pc files

---
+++ Session2_variable/var_1.js
@@ -0,0 +1,36 @@
+// console.log("Hello Wolrd!");
+// console.log("My name is Linh Doan.");
+// console.log("I am 24 years old.");
+// console.log("I am living in Hanoi.");
+
+// let name = "Linh";
+// console.log(name);
+// var age = 20;
+// console.log(age);
+// let a;
+// console.log(a);
+// const pi = 3.14;
+// console.log(pi);
+
+// let a = 10;
+// let b = 30;
+
+// let tong = a + b;
+// console.log(tong);
+
+// let hieu = a - b;
+// console.log(hieu);
+
+// let tich = a * b;
+// console.log(tich);
+
+// let thuong = a / b;
+// console.log(thuong);
+
+// console.log(a++);
+// console.log(a);
+
+// let name = prompt("Nhập vào họ tên của bạn: ");
+// let age = prompt("Nhập vào tuổi của bạn: ");
+// let result = `Xin chào ${name}, ${age} tuổi.`;
+// console.log(result);
---
+++ Sesssion4_condition/condition_intro.js
@@ -0,0 +1,21 @@
+// let a = '20';
+// let b = 20;
+// let soSanhBang = a === b;
+// console.log(soSanhBang);
+// let c = 30;
+
+// let phepVa = (a > b) && (a > c);
+// console.log(phepVa);
+
+// let a = parseInt(prompt("Mời bạn nhập vào a: "));
+// let b = parseInt(prompt("Mời bạn nhập vào b: "));
+// if (a > b)
+//     console.log("a lớn hơn b!");
+
+// let a = parseInt(prompt("Mời bạn nhập vào số cần kiểm tra: "));
+// if (a % 2 === 0) {
+//     console.log("Số này là số chẵn.");
+// }
+// else {
+//     console.log("Số này là số lẻ.");
+// }
