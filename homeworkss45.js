36d21252b05252ac4990d51435154c67be29990b
Linh Doan <doanvulinh310895@gmail.com>
2019-10-08 13:08:55 +0700

    xoa file cu ss4

--- homeworkss45.js
+++ homeworkss45.js
@@ -1,12 +1,38 @@
 /* 
-let n = parseInt(prompt("Moi nhap vao so N: "));
-for (let index = n; ((index >= 0) && (index % 2 === 0)); index -= 2) {
-    console.log(index);
+let passWord = prompt("Moi nhap vao mat khau cua ban: ");
+while (passWord.length < 8) {
+    passWord = prompt(`Mat khau chua du 8 ky tu. Moi nhap lai.`);
 }
+console.log(`Mat khau cua ban la: ${passWord}.
+Tai khoan cua ban da bi hack!`);
  */
 
-let n = parseInt(prompt("Moi nhap vao so N: "));
-let tong = 0;
-for (let index = 0; index < n; index++) {
-    tong += index;
+/* 
+let passWord = prompt("Moi nhap vao mat khau cua ban: ");
+while (passWord.length < 8) {
+    passWord = prompt("Mat khau chua du 8 ky tu. Moi nhap lai.");
 }
+
+let check = true;
+
+do {
+    for (let i = 0; i < passWord.length; i++) {
+        if (passWord.charAt(i) === '.'
+            || passWord.charAt(i) === '@'
+            || passWord.charAt(i) === '#') {
+            check = true;
+            break;
+        }
+        else {
+            check = false;
+        }
+    }
+    if (check === true) {
+        break;
+    }
+    else {
+        passWord = prompt("Mat khau phai chua it nhat 1 ky tu dac biet (. @ #). Moi nhap lai.");
+    }
+} while (check === false);
+console.log(`Mat khau cua ban la: ${passWord}. Tai khoan cua ban da bi hack!`);
+ */
