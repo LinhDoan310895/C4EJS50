ddf1164cda2592f7f5901ecf8f6dffe53fc43105
Linh Doan <doanvulinh310895@gmail.com>
2019-10-16 13:18:13 +0700

    update hwss7 tu pc

---
+++ homeworkss7.js
@@ -0,0 +1,21 @@
+// 1
+let person = {
+    'name': 'Pham Van A',
+    'age': 20,
+    'subject': ['Javascript', 'Html', 'css'],
+}
+// 2
+person.school = 'DH Quoc Gia HN';
+// 3
+let newKey = prompt("Moi nhap mot key moi: ");
+let newValue = [];
+let isEmpty = false;
+do {
+    newValue.push(prompt("Moi nhap vao value cua key moi (nhap 0 neu khong muon nhap them): "));
+    if (newValue[newValue.length] == '0') {
+        isEmpty = true;
+        newValue.pop();
+    }
+} while (isEmpty == false);
+person[newKey] = newValue;
+console.log(person);
